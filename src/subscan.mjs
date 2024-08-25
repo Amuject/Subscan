92190b983683c790d6748a99d82bbecf0e697208
Wany <sung@wanyne.com>
2024-08-25 17:26:45 +0000

    code cleanup

--- src/subscan.mjs
+++ src/subscan.mjs
@@ -1,21 +1,21 @@
 import fs from 'fs';
-import http from 'http';
-import https from 'https';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
 const ___filename = fileURLToPath(import.meta.url);
 const ___dirname = path.dirname(___filename);
-const scanURIs = JSON.parse(
-  fs.readFileSync(path.resolve(___dirname, '../data/scan-URIs.json'))
+const urisJson = JSON.parse(
+  fs.readFileSync(path.resolve(___dirname, '../data/uris.json'))
 );
+
+import fetch from 'node-fetch';
 
 /**
  * @function scan
  * @desc Scan subdomains of domain
  * @param {string} domain
  */
-async function scan(domain, URIs = scanURIs) {
+async function scan(domain, uris = urisJson) {
   const result = {
     error: undefined,
     domain: domain,
@@ -23,11 +23,7 @@ async function scan(domain, URIs = scanURIs) {
   };
   const tasks = [];
 
-  for (let uri of URIs) {
-    if (uri.match(/^#/)) {
-      continue;
-    }
-    uri = uri.replace(/{domain}/g, domain);
+  for (let uri of uris) {
     tasks.push(at(uri, domain));
   }
 
@@ -56,56 +52,28 @@ async function scan(domain, URIs = scanURIs) {
  * @desc Find target at uri
  * @param {string} uri
  * @param {string} target
- * @param {string} method default GET
+ * @param {string} domain default GET
  * @param {string} ua User-Agent default Sans
  */
-async function at(uri, target, method = 'GET', ua = 'Sans') {
-  return new Promise((resolve, reject) => {
-    target = encodeURI(target);
-    const regex = new RegExp(
-      '([a-zA-Z0-9_-][a-zA-Z0-9_.-]*\\.' + target.replace(/\./g, '\\.') + ')',
-      'ig'
-    );
-    const urim = uri.match(/(https?):\/\/([^/]+)(.+)/);
-    const proc = urim[1];
-    const host = urim[2];
-    const path = urim[3];
-    const timestamp = new Date().getTime();
-    const subdomains = [];
-    (proc == 'http' ? http : https)
-      .request(
-        {
-          method: method,
-          host: host,
-          path: path,
-          headers: {
-            'User-Agent': ua,
-          },
-        },
-        (response) => {
-          let d = '';
-          response.on('data', (data) => {
-            d += data.toString();
-          });
-          response.on('end', () => {
-            d = d.replace(/(\\n|\\r)/gi, ' ');
-            const matches = d.match(regex) || [];
-            for (const m of matches) {
-              //m = m.toUpperCase();
-              if (!subdomains.includes(m)) {
-                subdomains.push(m);
-              }
-            }
-            //subdomains.length > 0 ? log.debug('Find ' + subdomains.length + ' ' + target + ' subdomains at ' + host + ' (' + (new Date().getTime() - timestamp) + 'ms)') : null;
-            resolve(subdomains);
-          });
-        }
-      )
-      .on('error', (error) => {
-        //log.error(host + ' ' + error);
+async function at(uri, domain) {
+  const subdomains = [];
+  uri = uri.replace(/{domain}/g, domain);
+
+  return new Promise((resolve) => {
+    fetch(uri, { signal: AbortSignal.timeout(5000) })
+      .then(async (res) => {
+        const data = await res.text();
+        domain = encodeURI(domain);
+        domain = domain.replace(/\./g, '\\.');
+        const regex = new RegExp(`([\\w\\d.-]+\\.${domain})`, `ig`);
+        const matches = data.match(regex) || [];
+        subdomains.push(...matches);
         resolve(subdomains);
       })
-      .end();
+      .catch((error) => {
+        console.error(error);
+        resolve(subdomains);
+      });
   });
 }
 
---
+++ test/test.mjs
@@ -0,0 +1,3 @@
+import subscan from '../src/index.mjs';
+
+subscan('wnynya.com').then(console.log).catch(console.error);
